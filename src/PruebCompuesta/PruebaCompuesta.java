4117c79d07f3747d1ad4034d1c737ff1804e3eef
Kevin MoRo <125381456+Kevin-MR-xd@users.noreply.github.com>
2023-04-24 22:55:18 -0600

    Add files via upload

---
+++ src/PruebCompuesta/PruebaCompuesta.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PruebCompuesta;
+import java.util.Scanner;
+/**
+ *
+ * @author KEVIN
+ */
+public class PruebaCompuesta {
+    Scanner input = new Scanner(System.in);
+    int nota;
+    public void prueba(){
+        System.out.print("Ingresa la calificación (1-10), presiona enter: ");
+        nota=input.nextInt();
+        switch (nota){
+            case 10:
+            case 9:
+                System.out.println("Excelente");
+                break;
+            case 8:
+            case 7:
+                System.out.println("Notable");
+                break;
+            case 6:
+                System.out.println("Aprobado");
+                break;
+            case 5:
+                System.out.println("EReprobado");
+                break;
+            case 4:
+            case 3:
+            case 2:
+            case 1:
+            case 0:
+                System.out.println("Suspendido");
+                break;
+            default:
+                System.out.println("No es posible esta nota: ");
+        }
+        System.out.println("Fin del programa");
+    }
+    public static void main (String[]args){
+        PruebaCompuesta prueb = new PruebaCompuesta();
+        prueb.prueba();
+    }
+}
